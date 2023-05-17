235798897b92fca417bfd52fd27239dce1752a4b
prince Lee <princeleepraise@gmail.com>
2023-05-17 17:23:58 +0100

    update card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Table from '../components/Table';
+import { ethers } from "ethers";
+import ABI from '../ABI';
 
 const customizeStyle = {
   backgroundColor: "#81C3D7",
@@ -10,8 +12,9 @@ const customizeStyle = {
 
 function Dashboard() {
   const [burnPercentage, setBurnPercentage] = useState(0);
-
   const [totalBurn, setTotalBurn] = useState(0);
+  const [totalSupply, setTotalSupply] = useState("10,000,000,000");
+  const [circulatingSupply, setCirculatingSupply] = useState("N/A");
 
   const handleTotalValue = (value) => {
     setTotalBurn(value);
@@ -19,7 +22,12 @@ function Dashboard() {
 
   useEffect(() => {
     fetchBurnPercentage();
+    fetchTotalSupply();
   }, []);
+
+  useEffect(() => {
+    calculateCirculatingSupply();
+  }, [totalSupply, burnPercentage]);
 
   const fetchBurnPercentage = async () => {
     try {
@@ -33,16 +41,39 @@ function Dashboard() {
           apikey: '2KBH34YNQ4QHV4E5RHF5Y4QBCDWG5RJ5IP'
         }
       });
-      
-      // const totalSupply = 10000000000; // Replace with the actual total supply value
+
       const burnValue = response.data.result;
-      const percentage = (burnValue / 10000000000000000).toFixed(2);
-      
+      const percentage = ethers.utils.formatUnits(burnValue, 18);
 
       setBurnPercentage(percentage);
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const fetchTotalSupply = async () => {
+    try {
+      const provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.alchemyapi.io/v2/UAKNq4s1ggGd_t8Fm-CC4s2Z-iCX9U7G');
+      // Replace 'YOUR_ALCHEMY_API_KEY' with your actual Alchemy API key
+  
+      const contract = new ethers.Contract('0x679ec51c3989de468ce7b47ca6560636744f5ac6', ABI, provider);
+      // Replace 'CONTRACT_ADDRESS' with the actual contract address
+  
+      const totalSupply = await contract.totalSupply();
+  
+      setTotalSupply(totalSupply);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const calculateCirculatingSupply = () => {
+    const totalSupplyNum = parseFloat(totalSupply);
+    const burnPercentageNum = parseFloat(burnPercentage);
+    const totalBurnNum = parseFloat(totalBurn);
+
+    const circulatingSupplyNum = totalSupplyNum - (totalSupplyNum * burnPercentageNum / 100) - totalBurnNum;
+    setCirculatingSupply(circulatingSupplyNum);
   };
 
   return (
@@ -62,11 +93,11 @@ function Dashboard() {
             </div>
             <div>
               <h6>Total Supply</h6>
-              <p>10,000,000,000</p>
+              <p>{totalSupply}</p>
             </div>
             <div>
               <h6>Circulating Supply</h6>
-              <p>...</p>
+              <p>{circulatingSupply}</p>
             </div>
           </div>
 
@@ -78,7 +109,6 @@ function Dashboard() {
                 <div className="progress-bar bg-danger" style={{ width: `${burnPercentage}%` }}></div>
               </div>
             </div>
-            
           </div>
         </div>
         <div className="col-12 col-lg-8 burn-history">
